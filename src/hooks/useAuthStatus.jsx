9e4d85da3a79c1601fe07abaae592ad31dea23a5
elameen37 <elameen37@gmail.com>
2023-09-14 13:47:36 +0100

    [create] user  authentication status

---
+++ src/hooks/useAuthStatus.jsx
@@ -0,0 +1,22 @@
+import { getAuth, onAuthStateChanged } from "@firebase/auth";
+import { useEffect, useState } from "react";
+
+export function useAuthStatus() {
+    const [loggedIn, setLoggedIn] = useState(false)
+    const [checkingStatus, setCheckingStatus] = useState(true)
+
+    useEffect(()=> {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if(user) {
+                setLoggedIn(true);
+                // setCheckingStatus(false)
+            }
+                // setLoggedIn(false)
+                setCheckingStatus(false);
+            });
+        }, []);
+    
+  return { loggedIn, checkingStatus };
+
+}
