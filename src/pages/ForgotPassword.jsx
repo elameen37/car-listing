1b17f8e500ef523604382e9c31c0c2428d177973
Babalola Raphael Kehinde <kennybabs100@gmail.com>
2023-09-14 16:40:14 +0000

    functionality for forgotPassword

---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { toast } from "react-toastify";
+
+export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+
+  function onChange(e) {
+    setEmail(e.target.value);
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth()
+      await sendPasswordResetEmail(auth, email)
+      toast.success("Reset password email sent");
+    } catch (error) {
+      toast.error("Error sending reset email");
+    }
+  }
+  return (
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Forgot Password</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 mx-w-6xl mx-auto">
+        <div className="md:w-[60%] lg:w-[45%] mb-12 md:mb-6">
+          <img
+            src="https://images.unsplash.com/photo-1633265486064-086b219458ec?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+            alt="road"
+            className="w-full rounded-2xl"
+          />
+        </div>
+        <div className="mb-6 w-full md:w-[60%] lg:w-[40%] lg:ml-20">
+          <form onSubmit={onSubmit}>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email address"
+              className="w-full px-4 py-2 text-base text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+            />
+
+            <div className="flex items-center justify-between whitespace-nowrap text-base sm:text-sm">
+              <p className="mb-6">
+                Don't have an account?
+                <Link
+                  to="/sign-up"
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+                >
+                  Register
+                </Link>
+              </p>
+              <p className="mb-6">
+                <Link
+                  to="/sign-in"
+                  className="text-blue-600 hover:text-blue-700 transition duration-200 ease-in-out"
+                >
+                  Sign in instead
+                </Link>
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+            >
+              Send reset Password
+            </button>
+            <div className="flex items-center my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+            <OAuth />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
