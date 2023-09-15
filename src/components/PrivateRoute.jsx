16dcef9fcaa16a7f0228ef05311bf217447e1d69
Babalola Raphael Kehinde <kennybabs100@gmail.com>
2023-09-15 09:25:03 +0000

    new

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,11 +1,1 @@
-import { Outlet, Navigate } from "react-router-dom";
-import {useAuthStatus} from "../hooks/useAuthStatus";
-import Spinner from "./Spinner";
 
-export default function PrivateRoute() {
-  const { loggedIn, checkingStatus } = useAuthStatus();
-  if(checkingStatus) {
-    return <Spinner />; 
-  }
-  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
-}
