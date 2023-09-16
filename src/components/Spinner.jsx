81c5b5f19659984b2bc6fb9c6e374f53906770e5
DevKaffy <kafilatadewumi61@gmail.com>
2023-09-16 07:02:06 +0100

    assets: spinner.svg

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,4 +1,4 @@
-import spinner from "../../assets/svg/spinner.svg";
+import spinner from "../assets/svg";
 export default function Spinner() {
   return (
     <div className="bg-black bg-opacity-50 flex items-center justify-center fixed left-0 right-0 bottom-0 top-0 z-50">
