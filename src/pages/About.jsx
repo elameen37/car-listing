a942d0a9989d7e4bf0a1aa4e0022a11852c0da9e
DevKaffy <kafilatadewumi61@gmail.com>
2023-09-26 12:48:32 +0100

    fixes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import github from '../assets/svg/github.svg'
+import hyndai from '../assets/hyndai.jpeg'
 
 export const About = () => {
   return (
     <main className="p-8">
-      <h1 className="text-3xl text-center font-bold mb-4">About Us</h1>
+      <h1 className="text-3xl text-center font-bold mb-8">About Us</h1>
       <div className="flex justify-between flex-wrap items-start px-6 mx-w-6xl mx-auto mb-12">
         <div className="w-[50%] text-2xl leading-10">
-          Superkarz is a car listing website that makes it easy to compare
-          prices, features, and availability for all types of cars. With our
-          website, users can search for a car based on their specific needs and
-          preferences, such as price range, location, and car features. We have
-          also provided a platform for users to connect with each other and
-          share their experiences with car rental or buying.
+          <img className="w-full" src={hyndai} alt="" />
         </div>
         <div className="w-[40%] text-2xl leading-10">
-          So, if you're looking for the best way to find and rent or buy a car,
-          we invite you to check out our website. With our easy-to-use platform,
-          you'll be able to find the perfect car for your needs. We believe that
-          our solution will change the way people think about renting and buying
-          cars.
+          <p>
+            Superkarz is a car listing website that makes it easy to compare prices, features, and availability for all types of cars. With our website, users can search for a car based on their specific needs and preferences, such as price range, location, and car features. We
+            have also provided a platform for users to connect with each other and share their experiences with car rental or buying.
+          </p>
+          <p>
+            So, if you're looking for the best way to find and rent or buy a
+            car, we invite you to check out our website. With our easy-to-use
+            platform, you'll be able to find the perfect car for your needs. We
+            believe that our solution will change the way people think about
+            renting and buying cars.
+          </p>
         </div>
       </div>
       <h1 className="text-3xl text-center font-bold mb-8">TEAM</h1>
